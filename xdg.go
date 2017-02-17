e94264ce78fe99cf3a89b6d24d00a29619c2c510
Koichi Shiraishi <zchee.io@gmail.com>
2017-02-17 12:57:33 +0900

    xdg: add TODO comment for colon(:) seperated return value
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- xdg.go
+++ xdg.go
@@ -67,6 +67,8 @@ func ConfigHome() string {
 // $XDG_DATA_DIRS defines the preference-ordered set of base directories to search for data files in addition
 // to the $XDG_DATA_HOME base directory. The directories in $XDG_DATA_DIRS should be seperated with a colon ':'.
 // If $XDG_DATA_DIRS is either not set or empty, a value equal to /usr/local/share/:/usr/share/ should be used.
+// TODO(zchee): XDG_DATA_DIRS should be seperated with a colon, We should change return type to the []string
+// which colon seperated value instead of string?
 func DataDirs() string {
 	if dataDirs := os.Getenv("XDG_DATA_DIRS"); dataDirs != "" {
 		return dataDirs
@@ -79,6 +81,8 @@ func DataDirs() string {
 // $XDG_CONFIG_DIRS defines the preference-ordered set of base directories to search for configuration files in addition
 // to the $XDG_CONFIG_HOME base directory. The directories in $XDG_CONFIG_DIRS should be seperated with a colon ':'.
 // If $XDG_CONFIG_DIRS is either not set or empty, a value equal to /etc/xdg should be used.
+// TODO(zchee): XDG_CONFIG_DIRS should be seperated with a colon, We should change return type to the []string
+// which colon seperated value instead of string?
 func ConfigDirs() string {
 	if configDirs := os.Getenv("XDG_CONFIG_DIRS"); configDirs != "" {
 		return configDirs
